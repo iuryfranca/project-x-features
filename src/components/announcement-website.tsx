5b9bc8c0b6e8901454a26a619832aec9ea5037d0
Iury Franca <iurygfranca@gmail.com>
2023-03-25 17:42:55 -0400

    style: Card Announcement Website styleded💎

--- src/components/announcement-website.tsx
+++ src/components/announcement-website.tsx
@@ -14,41 +14,50 @@ export const AnnouncementWebsite = () => {
           <h1 className="text-5xl font-black">Project X</h1>
           <span className="text-sm">by Iury França</span>
         </div>
-        <span>
-          Faça compras, save seus items no carrinho e favorite os preferidos!
-        </span>
+        <span>Faça compras, salve e favorite seus produtos preferidos!</span>
         <Button className="flex gap-2 rounded-md">
           Conheça agora!
           <Icons.blankLink />
         </Button>
       </CardAnnouncementWebsite>
       <div className="flex w-full max-w-[340px] flex-col gap-5">
-        <CardAnnouncementWebsite className="h-full">
-          <LogIn size={40} />
-          Faça Login
-          <span>- com o login vc mantém seu carrinho salvo</span>
+        <CardAnnouncementWebsite className="flex h-full justify-center gap-10">
+          <div className="flex flex-col items-center justify-center gap-2">
+            <LogIn size={40} />
+            <h1 className="text-xl font-semibold">Faça Login</h1>
+          </div>
+          <span className="text-slate-600 dark:text-slate-400">
+            Com o login você mantém seu carrinho salvo
+          </span>
         </CardAnnouncementWebsite>
-        <CardAnnouncementWebsite className="h-4/5">
-          <ListChecks size={40} />
-          Lista de Pedidos
-          <span>tenha acesso a sum histórico de pedidos</span>
+        <CardAnnouncementWebsite className="flex h-4/5 justify-center gap-10">
+          <div className="flex flex-col items-center justify-center gap-2">
+            <ListChecks size={40} />
+            <h1 className="text-xl font-semibold">Lista de Pedidos</h1>
+          </div>
+          <span className="text-slate-600 dark:text-slate-400">
+            Tenha acesso a um histórico de pedidos
+          </span>
         </CardAnnouncementWebsite>
       </div>
       <div className="flex w-full max-w-[340px] flex-col gap-5">
-        <CardAnnouncementWebsite className="h-4/5">
-          <Heart size={40} />
-          Favorite produtos
-          <span>
-            gostou de algum produto? favorite ele para poder retornar a sua
+        <CardAnnouncementWebsite className="flex h-4/5 justify-center gap-10">
+          <div className="flex flex-col items-center justify-center gap-2">
+            <Heart size={40} />
+            <h1 className="text-xl font-semibold">Favorite produtos</h1>
+          </div>
+          <span className="text-slate-600 dark:text-slate-400">
+            Gostou de algum produto? Favorite ele para poder retornar a sua
             pagina posteriormente
           </span>
         </CardAnnouncementWebsite>
-        <CardAnnouncementWebsite className="h-full">
-          <UserCog size={40} />
-          Permissões de ADM
-          <span>
-            - se vc for ADM poderá ter acesso a diversa configurações dep
-            permissões de usurários
+        <CardAnnouncementWebsite className="flex h-full justify-center gap-10">
+          <div className="flex flex-col items-center justify-center gap-2">
+            <UserCog size={40} />
+            <h1 className="text-xl font-semibold">Permissões de ADM</h1>
+          </div>
+          <span className="text-slate-600 dark:text-slate-400">
+            ADMs terá acesso a diversas configurações e permissões de usurários
           </span>
         </CardAnnouncementWebsite>
       </div>
